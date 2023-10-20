3a22ef5dc4fe7c76209e8a76af481a9d721f1d2a
kazim110 <kazim.mohammadi110@gmail.com>
2023-10-20 20:39:48 +0430

    Make test for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -25,4 +25,30 @@ RSpec.describe Solver do
       expect(solver.reverse('hello')).to eq('olleh')
     end
   end
+
+  describe '#fizzbuzz' do
+  it "returns 'fizz' for multiples of 3" do
+    solver = Solver.new
+    expect(solver.fizzbuzz(3)).to eq('fizz')
+    expect(solver.fizzbuzz(6)).to eq('fizz')
+  end
+
+  it "returns 'buzz' for multiples of 5" do
+    solver = Solver.new
+    expect(solver.fizzbuzz(5)).to eq('buzz')
+    expect(solver.fizzbuzz(10)).to eq('buzz')
+  end
+
+  it "returns 'fizzbuzz' for multiples of both 3 and 5" do
+    solver = Solver.new
+    expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+  end
+
+  it 'returns the number itself for other cases' do
+    solver = Solver.new
+    expect(solver.fizzbuzz(1)).to eq('1')
+    expect(solver.fizzbuzz(7)).to eq('7')
+  end
+  end
 end
